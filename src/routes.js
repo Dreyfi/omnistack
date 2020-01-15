e76a3d9bc5497554b84d5d4d4438c626c6c38a39
Dreyfi F <dreyfi.sferreira@gmail.com>
2020-01-15 01:50:31 -0300

    Get, Post and Search devs

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 const { Router } = require('express');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
+
 const routes = Router();
 
 // Query params
 // Route params
 // Body
 
-routes.post('/users', (request, res) => {
-    console.log(request.body);
-    return res.json({message:'Hello omnistack.'});
-});
+routes.get('/devs', DevController.index);
+routes.post('/devs',  DevController.store);
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
