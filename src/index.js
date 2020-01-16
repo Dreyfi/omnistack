72bb5897f471e1b6a94413de80486742bfa0ae02
Dreyfi F <dreyfi.sferreira@gmail.com>
2020-01-16 01:22:39 -0300

    Adding cors

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
 const dotenv = require('dotenv');
+const cors = require('cors');
 const routes = require('./routes')
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 dotenv.config();
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-ar73x.mongodb.net/omnistack?retryWrites=true&w=majority`,
