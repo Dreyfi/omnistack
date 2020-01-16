5fc0ca45cacfbbaf11f7cc157a3399c98f096f96
Dreyfi F <dreyfi.sferreira@gmail.com>
2020-01-16 01:51:37 -0300

    Fixing cors bug

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const app = express();
 const dotenv = require('dotenv');
 const cors = require('cors');
-const routes = require('./routes')
+const routes = require('./routes');
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors());
 dotenv.config();
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-ar73x.mongodb.net/omnistack?retryWrites=true&w=majority`,
